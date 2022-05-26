1379c828675ff90e5ddd7c453acbb8e8c3689846
yogiwirmanda <wirmanda@gmail.com>
2022-05-26 13:01:26 +0700

    update dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ruangan;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,12 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $ruangan = Ruangan::select('nama')->where('status', 1)->get();
+        $dataRuangan = [];
+        foreach ($ruangan as $ruang) {
+            $dataRuangan[] = $ruang->nama;
+        }
+
+        return view('home', ['ruangan' => json_encode($dataRuangan)]);
     }
 }
